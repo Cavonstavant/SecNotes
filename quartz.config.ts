445d895adeae338d95bd52d864a3d23c78b95fc3
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2023-09-27 00:00:58 +0200

    Update baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,7 +7,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
-    baseUrl: "cavonstavant.github.io/SecNotes",
+    baseUrl: "cavonstavant.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
